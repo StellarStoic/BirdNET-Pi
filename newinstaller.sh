ec1f3d7eed5f02ed17861285f9f5131614750f1a
StellarStoic <stellarstoic@tuta.io>
2025-11-16 00:13:27 +0100

    fix(installer): update newinstaller.sh to clone from StellarStoic/BirdNET-Pi instead of Nachtzuster

--- newinstaller.sh
+++ newinstaller.sh
@@ -43,8 +43,10 @@ if [[ ! -z $PACKAGES_MISSING ]] ; then
   sudo apt -y install $PACKAGES_MISSING
 fi
 
-branch=main
 git clone -b $branch --depth=1 https://github.com/Nachtzuster/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
+
+branch=main
+git clone -b $branch --depth=1 https://github.com/StellarStoic/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
 
 $HOME/BirdNET-Pi/scripts/install_birdnet.sh
 if [ ${PIPESTATUS[0]} -eq 0 ];then
